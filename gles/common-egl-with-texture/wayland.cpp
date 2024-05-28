3813db482f51a594eda02966300d6b9b71475497
Besto Apache <bestoapache@gmail.com>
2024-05-28 13:24:47 +0800

    drop wl_shell

--- gles/common-egl-with-texture/wayland.cpp
+++ gles/common-egl-with-texture/wayland.cpp
@@ -8,14 +8,57 @@
 #include <wayland-egl.h>
 #include <linux/input.h>
 
+#include "xdg-shell-client-protocol.h"
+
 bool gShouldStop = false;
 
-wl_display* wlDisplay = NULL;
-wl_registry* wlRegistry = NULL;
-wl_compositor* wlCompositor = NULL;
-wl_shell* wlShell = NULL;
-wl_surface* wlSurface = NULL;
-wl_shell_surface* wlShellSurface = NULL;
+wl_display* wlDisplay = nullptr;
+wl_registry* wlRegistry = nullptr;
+wl_compositor* wlCompositor = nullptr;
+xdg_wm_base* xdgWmBase = nullptr;
+xdg_surface* xdgSurface = nullptr;
+wl_surface* wlSurface = nullptr;
+xdg_toplevel* xdgToplevel = nullptr;
+bool wait_for_configure = true;
+
+int windowWidth = 0;
+int windowHeight = 0;
+
+void xdg_surface_handle_configure(void * /*data*/, struct xdg_surface *xdg_surface, uint32_t serial)
+{
+    xdg_surface_ack_configure(xdg_surface, serial);
+    wait_for_configure = false;
+}
+
+const struct xdg_surface_listener xdg_surface_listener = {
+    xdg_surface_handle_configure,
+};
+
+void xdg_wm_base_handle_ping(void * /*data*/, struct xdg_wm_base *xdg_wm_base, uint32_t serial)
+{
+    xdg_wm_base_pong(xdg_wm_base, serial);
+}
+
+const struct xdg_wm_base_listener xdg_wm_base_listener = {
+    xdg_wm_base_handle_ping,
+};
+
+void xdg_toplevel_handle_configure(void * /*data*/, struct xdg_toplevel * /*xdg_toplevel*/, int32_t width, int32_t height, struct wl_array * /*states*/)
+{
+    fprintf(stderr, "configure event: width %d, height %d\n", width, height);
+    if (width != 0 && height !=0)
+    {
+        windowWidth = width;
+        windowHeight = height;
+    }
+}
+
+const struct xdg_toplevel_listener xdg_toplevel_listener = {
+    xdg_toplevel_handle_configure,
+    nullptr,
+    nullptr,
+    nullptr,
+};
 
 static void registerGlobalCallback(void* /*data*/, wl_registry* registry, uint32_t name, const char* interface, uint32_t /*version*/)
 {
@@ -23,9 +66,10 @@ static void registerGlobalCallback(void* /*data*/, wl_registry* registry, uint32
     {
         wlCompositor = (wl_compositor*)wl_registry_bind(registry, name, &wl_compositor_interface, 1);
     }
-    else if (strcmp(interface, "wl_shell") == 0)
+    else if (strcmp(interface, "xdg_wm_base") == 0)
     {
-        wlShell = (wl_shell*)wl_registry_bind(registry, name, &wl_shell_interface, 1);
+        xdgWmBase = (xdg_wm_base*)wl_registry_bind(registry, name, &xdg_wm_base_interface, 1);
+        xdg_wm_base_add_listener(xdgWmBase, &xdg_wm_base_listener, nullptr);
     }
 }
 
@@ -37,38 +81,22 @@ static const wl_registry_listener registryListener =
     globalObjectRemove
 };
 
-static void ping_cb(void* /*data*/, struct wl_shell_surface* shell_surface, uint32_t serial)
-{
-    wl_shell_surface_pong(shell_surface, serial);
-}
-
-static void configure_cb(void* /*data*/, struct wl_shell_surface* /*shell_surface*/, uint32_t /*edges*/, int32_t /*width*/, int32_t /*height*/) { }
-
-static void popupDone_cb(void* /*data*/, struct wl_shell_surface* /*shell_surface*/) { }
-
-static const struct wl_shell_surface_listener shellSurfaceListeners =
-{
-    ping_cb,
-    configure_cb,
-    popupDone_cb
-};
-
 bool initWaylandConnection()
 {
-    if ((wlDisplay = wl_display_connect(NULL)) == NULL)
+    if ((wlDisplay = wl_display_connect(nullptr)) == nullptr)
     {
         printf("Failed to connect to Wayland display!\n");
         return false;
     }
 
-    if ((wlRegistry = wl_display_get_registry(wlDisplay)) == NULL)
+    if ((wlRegistry = wl_display_get_registry(wlDisplay)) == nullptr)
     {
         printf("Faield to get Wayland registry!\n");
         return false;
     }
 
-    wl_registry_add_listener(wlRegistry, &registryListener, NULL);
-    wl_display_dispatch(wlDisplay);
+    wl_registry_add_listener(wlRegistry, &registryListener, nullptr);
+    wl_display_roundtrip(wlDisplay);
     if (!wlCompositor)
     {
         printf("Could not bind Wayland protocols!\n");
@@ -86,24 +114,36 @@ bool nativeInit(int w, int h, EGLNativeDisplayType *eglNativeDisplay, EGLNativeW
     *eglNativeDisplay = (EGLNativeDisplayType)wlDisplay;
 
     wlSurface = wl_compositor_create_surface(wlCompositor);
-    if (wlSurface == NULL)
+    if (wlSurface == nullptr)
     {
         printf("Failed to create Wayland surface\n");
         return false;
     }
 
-    wlShellSurface = wl_shell_get_shell_surface(wlShell, wlSurface);
-    if (wlShellSurface == NULL)
+    xdgSurface = xdg_wm_base_get_xdg_surface(xdgWmBase, wlSurface);
+    if (xdgSurface == nullptr)
     {
         printf("Failed to get Wayland shell surface\n");
         return false;
     }
+    xdg_surface_add_listener(xdgSurface, &xdg_surface_listener, nullptr);
 
-    wl_shell_surface_add_listener(wlShellSurface, &shellSurfaceListeners, NULL);
+    xdgToplevel = xdg_surface_get_toplevel(xdgSurface);
+    xdg_toplevel_set_title(xdgToplevel, "simple-egl");
+    xdg_toplevel_add_listener(xdgToplevel, &xdg_toplevel_listener, nullptr);
+
     if (fullscreen)
-        wl_shell_surface_set_fullscreen(wlShellSurface, WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT, 0, nullptr);
-    else
-        wl_shell_surface_set_toplevel(wlShellSurface);
+        xdg_toplevel_set_fullscreen(xdgToplevel, nullptr);
+    wl_surface_commit(wlSurface);
+
+    while (wait_for_configure)
+        wl_display_roundtrip(wlDisplay);
+
+    if (windowWidth != 0 && windowHeight != 0)
+    {
+        w = windowWidth;
+        h = windowHeight;
+    }
 
     *eglNativeWindow = (EGLNativeWindowType)wl_egl_window_create(wlSurface, w, h);
     if (*eglNativeWindow == (EGLNativeWindowType)EGL_NO_SURFACE) {
@@ -116,9 +156,9 @@ bool nativeInit(int w, int h, EGLNativeDisplayType *eglNativeDisplay, EGLNativeW
 
 void nativeDestroy()
 {
-    if (wlShellSurface) wl_shell_surface_destroy(wlShellSurface);
+    if (xdgSurface) xdg_surface_destroy(xdgSurface);
     if (wlSurface) wl_surface_destroy(wlSurface);
-    if (wlShell) wl_shell_destroy(wlShell);
+    if (xdgWmBase) xdg_wm_base_destroy(xdgWmBase);
     if (wlCompositor) wl_compositor_destroy(wlCompositor);
     if (wlRegistry) wl_registry_destroy(wlRegistry);
     if (wlDisplay) wl_display_disconnect(wlDisplay);
