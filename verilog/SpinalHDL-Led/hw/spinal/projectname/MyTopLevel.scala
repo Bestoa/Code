da3bc79ac70c10eb9b3d8804f2431eab276d2232
Besto Apache <bestoapache@gmail.com>
2024-10-14 12:06:44 +0800

    Add Spinal HDL PLL blackbox for Led demo

--- verilog/SpinalHDL-Led/hw/spinal/projectname/MyTopLevel.scala
+++ verilog/SpinalHDL-Led/hw/spinal/projectname/MyTopLevel.scala
@@ -2,23 +2,51 @@ package projectname
 
 import spinal.core._
 
+case class PLL() extends BlackBox {
+  val io = new Bundle {
+    val clkin = in Bool()
+    val clkout0 = out Bool()
+    val locked = out Bool()
+  }
+  noIoPrefix()
+}
+
 // Hardware definition
 case class Led() extends Component {
   val io = new Bundle {
     val led = out Bits(4 bits)
   }
 
-  val counter = Reg(UInt(32 bits)) init 0
-  counter := counter + 1;
+  // Create an Area to manage all clocks and reset things
+  val clkCtrl = new Area {
+    // Instantiate and drive the PLL
+    val pll = new PLL
+    pll.io.clkin := ClockDomain.current.readClockWire
 
-  val led = Reg(Bits(4 bits)) init 1
+    // Create a new clock domain named 'core'
+    val coreClockDomain = ClockDomain.internal(
+      name = "core",
+      frequency = FixedFrequency(48 MHz)  // This frequency specification can be used
+    )                                      // by coreClockDomain users to do some calculations
 
-  when (counter === 24_000_000) {
-    counter := 0
-    led := led(2 downto 0) ## led(3)
+    // Drive clock and reset signals of the coreClockDomain previously created
+    coreClockDomain.clock := pll.io.clkout0
+    coreClockDomain.reset := ClockDomain.current.readResetWire && pll.io.locked
   }
 
-  io.led := ~led
+  val core = new ClockingArea(clkCtrl.coreClockDomain) {
+    val counter = Reg(UInt(32 bits)) init 0
+    val led = Reg(Bits(4 bits)) init 1
+
+    counter := counter + 1;
+
+    when (counter === 24_000_000) {
+      counter := 0
+      led := led(2 downto 0) ## led(3)
+    }
+    io.led := ~led
+  }
+
 }
 
 object MyTopLevelVerilog extends App {
