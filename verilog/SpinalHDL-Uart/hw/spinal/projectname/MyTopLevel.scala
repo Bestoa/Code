a1e327b7e2fe9bebc4a6965774405b6466489d67
Besto Apache <bestoapache@gmail.com>
2024-10-16 15:27:27 +0800

    Refine SpinalHDL uart demo

--- verilog/SpinalHDL-Uart/hw/spinal/projectname/MyTopLevel.scala
+++ verilog/SpinalHDL-Uart/hw/spinal/projectname/MyTopLevel.scala
@@ -7,6 +7,7 @@ import spinal.lib.com.uart._
 case class UartCtrlUsageExample() extends Component {
   val io = new Bundle {
     val uart = master(Uart())
+    var led = out Bits(4 bits)
   }
 
   val uartCtrl = new UartCtrl()
@@ -20,19 +21,45 @@ case class UartCtrlUsageExample() extends Component {
   uartCtrl.io.writeBreak := False
   uartCtrl.io.read.ready := True
 
-  val writeValid = Reg(Bool()) init False
-
-  uartCtrl.io.write.payload := uartCtrl.io.read.payload
-
-  when (uartCtrl.io.read.valid) {
-    writeValid := True
+  val writeValid = Reg(Bool()) init True
+  val cnt = Reg(UInt(32 bits)) init 0
+  val enableEcho = Reg(Bool()) init False
+  val index = Reg(UInt(32 bits)) init 0
+  val hello_str = "Welcome to SpinalHDL's world-1!\n\r"
+  
+  val hello = Bits(hello_str.length*8 bits)
+  for (i <- 0 until hello_str.length) {
+    hello((i+1)*8-1 downto i*8) := hello_str(i)
   }
+
+
+  cnt := cnt + 1
+  when (cnt === 24_000_000) {
+    writeValid := True
+    cnt := 0
+    index := 0
+    enableEcho := False
+  }
+
+  when (enableEcho) {
+    uartCtrl.io.write.payload := uartCtrl.io.read.payload
+    writeValid.setWhen(uartCtrl.io.read.valid)
+  } otherwise {
+    uartCtrl.io.write.payload := hello((index)*8, 8 bits)
+  }
+
   when (uartCtrl.io.write.ready) {
-    writeValid := False
+    when (index < hello_str.length - 1) {
+      index := index + 1
+    } otherwise {
+      enableEcho := True
+      writeValid := False
+    }
   }
 
   uartCtrl.io.write.valid := writeValid
 
+  io.led := ~uartCtrl.io.read.payload(0, 4 bits)
 }
 
 object MyTopLevelVerilog extends App {
